d0a2d32334007e29c781208549d53eab84f73a0b
Frank Wazeter <38684085+fwazeter@users.noreply.github.com>
2024-03-17 23:39:58 -0700

    Updated Repel
    Decided to give Repel a toggle for vertical since that seems to be a usecase rather than new component.
    
    Toggle works because --OFF: ; is equivalent to unset with custom props, which will make the default value fire when: var(--_toggle, column); it defaults to row rather than column because it is invalid at run time.
    
    Toggling --ON (in global, --ON: initial; makes the custom prop valid again and causes the column fallback to initiate.
    
    Considering using data-toggle as a new pattern among other for bools.

--- src/repel/index.tsx
+++ src/repel/index.tsx
@@ -5,6 +5,7 @@ import {RepelProps} from './types.js';
 const Repel: React.FC<React.PropsWithChildren<RepelProps>> = ({
                                                                   space,
                                                                   align,
+                                                                  direction,
                                                                   tag: Tag = 'div',
                                                                   children,
                                                                   ...props
@@ -15,6 +16,7 @@ const Repel: React.FC<React.PropsWithChildren<RepelProps>> = ({
             className={styles.repel}
             data-space={space ? space : undefined}
             data-align={align ? align : undefined}
+            data-direction={direction ? true : undefined}
             {...props}
         >
             {children}
--- src/repel/types.d.ts
+++ src/repel/types.d.ts
@@ -2,4 +2,5 @@ import {HTMLProps, commonStylesAPI} from "../../types.js";
 
 export interface RepelProps extends HTMLProps, commonStylesAPI {
     align: string;
+    direction: boolean;
 }
